92b937149b59edf82d9c6b50f5554abcbff1385a
Anais-RV <anaisrv@gmail.com>
2023-10-19 17:30:08 +0200

    login

--- backend/app/routers/user_router.py
+++ backend/app/routers/user_router.py
@@ -1,12 +1,22 @@
-from fastapi import APIRouter, HTTPException, Depends, UploadFile, File
+# Bibliotecas estándar
+from datetime import datetime, timedelta
+import json
+import shutil
+
+# Bibliotecas de terceros
+from fastapi import APIRouter, HTTPException, Depends, UploadFile, File, Form
 from sqlalchemy.orm import Session
+from werkzeug.security import check_password_hash
+import jwt
+from jwt.exceptions import InvalidTokenError as JWTError
+
+
+
+# Importaciones específicas del módulo o aplicativo
 from ..database import get_db
 from ..models.user_model import User as UserModel
 from ..schemas.user_schema import User, UserCreate, UserLogin, UserResponse
-from werkzeug.security import check_password_hash
-from fastapi import Form
-from datetime import datetime, timedelta
-import shutil, json, jwt
+
 
 router = APIRouter()
 
@@ -56,4 +66,29 @@ def login(user: UserLogin, db: Session = Depends(get_db)):
     if not check_password_hash(db_user.hashed_password, user.password):
         raise HTTPException(status_code=400, detail="Invalid password")
     
-    return{"token": token, "user": db_user}
+    access_token = create_access_token(data={"sub": user.user_name})
+    return{"token": access_token, "user": db_user}
+
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
+
+def create_access_token(data: dict, expires_delta: timedelta = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+def verify_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_name: str = payload.get("sub")
+        if user_name is None:
+            raise HTTPException(status_code=400, detail="Token is invalid")
+        return user_name
+    except JWTError:
+        raise HTTPException(status_code=400, detail="Token is invalid")
